56b58d72db5e936b78471709278a7f1bce15e81e
Joshua Hinman <joshuahinman@gmail.com>
2023-10-06 17:34:27 -0700

    set db values for deploy env

--- utilities/db.js
+++ utilities/db.js
@@ -3,11 +3,19 @@ const { Pool } = pg;
 //import dotenv from 'dotenv';
 //dotenv.config();
 
+// const pool = new Pool ({
+//   user: 'postgres',
+//   password: 'ogamiitto1',
+//   host: 'localhost',
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME
+// });
+
 const pool = new Pool ({
-  user: 'postgres',
-  password: 'ogamiitto1',
-  host: 'localhost',
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
   database: process.env.DB_NAME
 });
 
